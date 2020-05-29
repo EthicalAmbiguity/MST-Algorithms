4fb142989a6942f585b3b643a9eba0ea5adcad2f
EthicalAmbiguity <nathaniel.j.li@gmail.com>
2020-05-29 08:58:40 -0700

    Implement Prim's algorithm
    Located in prim()

--- src/PrimAndKruskal.java
+++ src/PrimAndKruskal.java
@@ -10,13 +10,17 @@ import java.lang.*;
 public class PrimAndKruskal {
 
     private String filename;
+    private int numNodes;
+
 
     /* adjacency matrix for Prim's Algorithm */
     private int[][] adjacencyMatrix;
 
     private int[] parent;
     private int[] size;
-    private ArrayList<Edge> edges = new ArrayList<Edge>();
+
+    /* edge list for Kruskal's Algorithm */
+    private ArrayList<Edge> edges = new ArrayList<>();
 
     /**
      * @param filename the name of the file that contains the graph data. Requires formatting as described in the README.
@@ -46,7 +50,7 @@ public class PrimAndKruskal {
         Scanner in = new Scanner(new File(filename));
 
         /* the number of nodes is the first number, on the first line of the text file, as described in the README */
-        int numNodes = in.nextInt();
+        numNodes = in.nextInt();
         adjacencyMatrix = new int[numNodes][numNodes];
 
         /* loop through every combination of nodes */
@@ -56,7 +60,7 @@ public class PrimAndKruskal {
 
                 /* as described in the README, if the distance is zero, there is no connection */
                 if (distance != 0) {
-                    
+
                     /* translate the graph into an adjacency matrix for Prim's Algorithm */
                     adjacencyMatrix[i][j] = distance;
 
@@ -65,8 +69,43 @@ public class PrimAndKruskal {
                 }
             }
         }
+    }
 
-        System.out.println();
+    /**
+     * Utilizes Prim's algorithm for Minimum Spanning Trees on adjacencyMatrix
+     * @return the total size of the minimum spanning tree
+     */
+    public int prims() {
+        
+        int cur = 0;
+        int[] distances = new int[numNodes];
+        boolean[] visited = new boolean[numNodes];
+
+        /* initialize each distance with a large number; cannot be Integer.MAX_VALUE because
+        * it will overflow after relaxing
+        * */
+        Arrays.fill(distances, 1000000000);
+
+        distances[0] = 0; //replace array index with whatever the starting element is
+        for (int node = 0; node < numNodes; node++) { //MST with prims
+            visited[cur] = true;
+            for (int next = 0; next < numNodes; next++) {
+                if (adjacencyMatrix[cur][next] > 0 && adjacencyMatrix[cur][next] < distances[next] && !visited[next]) distances[next] = adjacencyMatrix[cur][next];
+            }
+            int min = Integer.MAX_VALUE;
+            for (int next = 0; next < numNodes; next++) {
+                if (distances[next] < min && !visited[next]) {
+                    cur = next;
+                    min = distances[next];
+                }
+            }
+        }
+
+        int total = 0;
+        for (int distance : distances) {
+            total += distance;
+        }
+        return total;
     }
 
     /**
